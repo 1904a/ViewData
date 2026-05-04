6a38fb8285d1d1c59a2bc1d9f2f367525cd4dbb2
Duy Tú <duytu19042001@gmail.com>
2026-05-04 13:41:59 +0700

    http

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://aybkkuhsmtzytnurpnjh.supabase.co'
+const supabaseUrl = 'http://aybkkuhsmtzytnurpnjh.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF5YmtrdWhzbXR6eXRudXJwbmpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUyMzMzNDMsImV4cCI6MjA5MDgwOTM0M30.2rojSxpGDEeapv7u1x5wJ_4ZTtdDMAD5F8_D2k6O_JI'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
